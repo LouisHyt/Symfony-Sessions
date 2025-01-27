b7f31f8724e6e4f3de2c1fbb074f8d21c697f7e1
Louis <louishayotte@gmail.com>
2025-01-27 09:51:56 +0100

    feat: Added popover for interns

--- code/public/js/internPanel.js
+++ code/public/js/internPanel.js
@@ -1,8 +1,25 @@
 const openActionsMenu = document.querySelectorAll(".open-actions");
 
 openActionsMenu.forEach((button) => {
-    button.addEventListener("click", () => {
-        const userId = icon.closest('tr').dataset.internId;
-        console.log(userId);
+    button.addEventListener("click", (e) => {
+        e.stopPropagation();
+        const userId = button.closest('tr').dataset.internId;
+        const popover = button.closest('.actions').querySelector(".popover");
+        popover.classList.toggle("visible");
+        document.addEventListener('click', handleCloseClick);
     });
 });
+
+
+function clearActivePopover() {
+    document.removeEventListener("click", handleCloseClick)
+    const activePopover = document.querySelector('.popover.visible');
+    if(!activePopover) return
+    activePopover.classList.remove("visible");
+}
+
+function handleCloseClick(e){
+    if(!e.target.closest(".popover")){
+        clearActivePopover();
+    }
+}
